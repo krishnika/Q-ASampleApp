27d2270a249a51b0f7bc9ff701b770390931b498
krishnika agarwal <krishnikaagarwal@krishnikas-MacBook-Pro.local>
2016-12-17 17:58:24 +0530

    Basic cell design

---
+++ QASampleApp/QASampleApp/QnAViewController.swift
@@ -0,0 +1,56 @@
+//
+//  QnAViewController.swift
+//  QASampleApp
+//
+//  Created by krishnika agarwal on 17/12/16.
+//  Copyright © 2016 krishnika agarwal. All rights reserved.
+//
+
+import UIKit
+
+class QnAViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension QnAViewController : UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "answerPost", for: indexPath) as UITableViewCell
+        
+        
+        
+        return cell
+    }
+    
+}
