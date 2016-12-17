6e7607c41c14052bfb02d2e41841f3789328f041
krishnika agarwal <krishnikaagarwal@krishnikas-MacBook-Pro.local>
2016-12-18 01:15:59 +0530

    UI Changes in Navigation View

---
+++ QASampleApp/QASampleApp/NavigationView.swift
@@ -0,0 +1,21 @@
+//
+//  NavigationView.swift
+//  QASampleApp
+//
+//  Created by krishnika agarwal on 18/12/16.
+//  Copyright © 2016 krishnika agarwal. All rights reserved.
+//
+
+import UIKit
+
+class NavigationView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
