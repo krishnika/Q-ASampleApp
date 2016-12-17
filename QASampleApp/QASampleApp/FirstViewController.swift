61d2ea345bd8f7efcb2dd79fd95302ff7ec60f46
krishnika agarwal <krishnikaagarwal@krishnikas-MacBook-Pro.local>
2016-12-17 17:43:43 +0530

    Initial commit

---
+++ QASampleApp/QASampleApp/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  QASampleApp
+//
+//  Created by krishnika agarwal on 17/12/16.
+//  Copyright © 2016 krishnika agarwal. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
